8869c899c1bdab9ae15a2a4d06aba927205e75f2
Muharem Hrnjadovic <muharem@linux.com>
2019-01-01 18:53:32 +0100

    wip: config module + tests

--- config.go
+++ config.go
@@ -1,6 +1,26 @@
+// gmt sends emails in bulk based on a template and a config file.
+// Copyright (C) 2019  "Muharem Hrnjadovic" <gmt@lbox.cc>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package main
 
 import (
+	"errors"
+	"regexp"
+	"strconv"
+
 	"github.com/go-ini/ini"
 )
 
@@ -27,13 +47,14 @@ type General struct {
 	EncryptAttachments bool
 	SenderEmail string
 	SenderName string
-	Cc string
+	Cc []string
 	Recipients []Recipient
 	Attachments []Attachment
 }
 
-func Make(bs []byte) (result General) {
-	cfg, err := ini.Load(bs)
+func NewConfig(bs []byte) (result General, err error) {
+	var cfg *ini.File
+	cfg, err = ini.Load(bs)
 	if err != nil {
 		return
 	}
@@ -41,35 +62,35 @@ func Make(bs []byte) (result General) {
 	if err != nil {
 		return
 	}
+	keys := sec.KeysHash()
+
 	// mandatory keys
-	key, err := sec.GetKey("mail-prog")
-	if err != nil {
+	if val, ok := keys["mail-prog"]; ok {
+		result.MailProg = val
+	} else {
+		err = errors.New("'mail-prog' not configured!")
 		return
 	}
-	result.MailProg = key.String()
+
 	// optional keys
-	key, err = sec.GetKey("attachment-path")
-	if err == nil {
-		result.AttachmentPath = key.String()
+	if val, ok := keys["attachment-path"]; ok {
+		result.AttachmentPath = val
 	}
-	key, err = sec.GetKey("encrypt-attachments")
-	if err == nil {
-		result.EncryptAttachments, err = key.Bool()
+	if val, ok := keys["encrypt-attachments"]; ok {
+		result.EncryptAttachments, err = strconv.ParseBool(val)
 		if err != nil {
-			result.EncryptAttachments = false
+			return
 		}
 	}
-	key, err = sec.GetKey("sender-email")
-	if err == nil {
-		result.SenderEmail = key.String()
+	if val, ok := keys["sender-email"]; ok {
+		result.SenderEmail = val
 	}
-	key, err = sec.GetKey("sender-name")
-	if err == nil {
-		result.SenderName = key.String()
+	if val, ok := keys["sender-name"]; ok {
+		result.SenderName = val
 	}
-	key, err = sec.GetKey("Cc")
-	if err == nil {
-		result.Cc = key.String()
+	if val, ok := keys["Cc"]; ok {
+		re := regexp.MustCompile("\\s*,\\s*")
+		result.Cc = re.Split(val, -1)
 	}
 	return
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/al-maisan/gmt
 
-require github.com/go-ini/ini v1.40.0
+require (
+	github.com/go-ini/ini v1.40.0
+	github.com/jtolds/gls v4.2.1+incompatible // indirect
+	github.com/smartystreets/assertions v0.0.0-20180927180507-b2de0cb4f26d // indirect
+	github.com/smartystreets/goconvey v0.0.0-20181108003508-044398e4856c
+)
